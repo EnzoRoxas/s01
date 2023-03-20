7cf5b114429405bbb5682fedb7b4bc24f7ce6e78
RalphEnzoBon <rrb1962@dlsud.edu.ph>
2023-03-20 17:34:47 +0700

    230320

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Welcome to De La Salle University!");
+
+let firstName = "Ralph Enzo";
+let lastName = "Roxas";
+let age = 20;
+let hobbies = ["Reading Novels", "Reading Manga", "Playing Games", "Watching Anime", "Watching TV Series & Movies", "Listening to Music"];
+let address ={
+   houseNumber : "Blk 9-C Lot 18",
+   street : "Congressional Road",
+   subdivision : "South Garden Homes",
+   barangay : "Salitran III",
+   city :"Dasmariñas City",
+   province : "Cavite",
+   zipCode : 4114
+};
+ console.log(firstName + " "+ lastName);
+ console.log(age);
+ console.log(hobbies);
+ console.log(address);
